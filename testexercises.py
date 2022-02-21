d7c8050408dfc480e82c57d28b68324266cba187
luctho27 <100096997+luctho27@users.noreply.github.com>
2022-02-22 12:19:55 +1300

    we did some test exercises to help us gain knowledge on python

---
+++ testexercises.py
@@ -0,0 +1,22 @@
+#print out name
+print("Lucas Thomson")
+#print out teachers name
+print("Luke Smith")
+#print out school name
+print("Kings high school")
+#print out age
+print("15")
+#print out hours in fav game
+print("468 hours")
+#print out year in 25 years
+print(2022+25)
+#print how old in 100 years
+print(15+100)
+#print ans 658/7
+print(658/7)
+#how many years first moon land ago
+print(2022-1969)
+#name of fav game
+print("AC Odyssey")
+#fav class
+print("P.E.")
