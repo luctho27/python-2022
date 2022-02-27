51591a27837aea36e61b56591ab7d1d9c6df163d
luctho27 <100096997+luctho27@users.noreply.github.com>
2022-02-28 09:12:27 +1300

    Create 2_data_types.py

---
+++ 2_data_types.py
@@ -0,0 +1,60 @@
+#1
+print("Go now\n you an rockstar\n get yo game on go play")
+#2
+print("\"go cityyyyyy\" - doc hudson")
+#3
+print("a string is a bunch of characters")
+#4
+print("a number")
+#5
+print("a float is numbers with a decimal")
+#6
+print(543/3)
+#7
+print(543//3)
+#8
+print(543%3)
+#9
+print(1000%9)
+#10
+print(434//10)
+#11
+print(3%2)
+#12
+print(5%5)
+#13
+print(555/2)
+#14
+print(123/3)
+#15
+print(123//2)
+#16
+print(123%2)
+#17
+print(764%2)
+#18
+print(165//2)
+#19
+float(1.3+2.4)
+#20
+int(1+2)
+#21
+int(3-float(2.0))
+#22
+float(3-int(2))
+#23
+int(10/int(5))
+#24
+float(5.0*int(5))
+#25
+int(2*int(2))
+#26
+print("lucas"*10)
+#27
+print("lucas"*10)
+#28
+print("5+10=" + str(5+10))
+#29
+print("15/2=" + str(15/2))
+#30
+print("30/2*5=" + str(30/2*5))
